40e53e8937f666d6fcee42845e83b68af2a86b2f
Vikash1804 <vikkyvikash8765s1@gmail.com>
2026-09-13 01:43:32 +0530

    Validate

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const UserModel = require("../models/User");
-const { ValidateSignUp } = require("../utils/validate");
+const { ValidateSignUp } = require("../utils/Validate");
 const AuthRouter = express.Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken")
